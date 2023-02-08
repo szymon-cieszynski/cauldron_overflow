345c64f225e352f7dde544925290dde83f2f42c6
szymon cieszynski <cieszynski8@gmail.com>
2023-02-08 12:26:12 +0100

    insert and fetching data from data basE

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
+use Knp\Bundle\TimeBundle\Templating\Helper\TimeHelper;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Sentry\State\HubInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 class QuestionController extends AbstractController
@@ -31,9 +35,40 @@ class QuestionController extends AbstractController
     }
 
     /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+        //dd($question);
+        $entityManager->persist($question);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug: %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
+    }
+
+    /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
-    public function show($slug, MarkdownHelper $markdownHelper)
+    public function show($slug, MarkdownHelper $markdownHelper, EntityManagerInterface $entityManager)
     {
 
         if($this->isDebug)
@@ -41,26 +76,32 @@ class QuestionController extends AbstractController
             $this->logger->info('Jestesmy w trybie debugowania');
         }
 
-        //throw new \Exception('bad stuff happend!');
-        //dump($isDebug);
-        //dump($this->getParameter('cache_adapter'));
+        $repository = $entityManager->getRepository(Question::class);
+        /** @var Question|null $question */
+        $question = $repository->findOneBy(['slug'=>$slug]);
+        if(!$question)
+        {
+            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
+        }
+
         $answers = [
             'Make sure your cat is sitting `purrrfectly` still ?',
             'Honestly, I like furry shoes better than MY cat',
             'Maybe... try saying the spell backwards?',
         ];
 
-        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
-        $parsedQuestionText = $markdownHelper->parse($questionText);
+//        $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+//        $parsedQuestionText = $markdownHelper->parse($questionText);
 
        //dump($cache);
 
         //dump($this);
 
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-',' ', $slug)),
-            'questionText' => $parsedQuestionText,
+            'question' => $question,
             'answers' => $answers,
         ]);
     }
+
+
 }
