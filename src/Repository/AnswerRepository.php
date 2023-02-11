d44f94b4043c3cd5a78fd69007b6e4cd805987c8
szymon cieszynski <cieszynski8@gmail.com>
2023-02-11 18:29:58 +0100

    creating Tag entity (ManyToMany relationship)

--- src/Repository/AnswerRepository.php
+++ src/Repository/AnswerRepository.php
@@ -85,13 +85,21 @@ class AnswerRepository extends ServiceEntityRepository
     /**
      * @return Answer[] Returns an array of Answer objects
      */
-    public function findMostPopular(): array
+    public function findMostPopular(string $search = null): array
     {
-        return $this->createQueryBuilder('answer')
+        $queryBuilder = $this->createQueryBuilder('answer')
             ->addCriteria(self::createApprovedCriteria())
             ->orderBy('answer.votes', 'DESC')
             ->innerJoin('answer.question', 'question')
-            ->addSelect('question')
+            ->addSelect('question');
+
+        if($search)
+        {
+            $queryBuilder->andWhere('answer.content LIKE :searchTerm OR question.question LIKE :searchTerm')
+                ->setParameter('searchTerm', '%'.$search.'%');
+        }
+
+        return $queryBuilder
             ->setMaxResults(10)
             ->getQuery()
             ->getResult();
